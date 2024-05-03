d9f9c8134e8024bba0b894c60abebeade6dbb0db
Emanuele <egentili2000@gmail.com>
2024-05-03 15:56:58 +0200

    Modified the failure detection method

--- registry/registry.go
+++ registry/registry.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"net"
 	"net/rpc"
 	"os"
-	"regexp"
 	"serviceRegistry/services"
 	"time"
 )
@@ -16,35 +14,37 @@ func main() {
 	registry := services.NewRegistry()
 	port := os.Getenv("PORT")
 	playOnRegistry(port, registry)
+	idleServer := createIdleServerList(registry.AvailableServer)
 	for {
-		for key, _ := range registry.AvailableServer {
-			conn, err := net.DialTimeout("tcp", key, 8*time.Second)
+		for _, s := range idleServer {
+			conn, err := net.DialTimeout("tcp", s, 5*time.Second)
 			if conn != nil {
 				_ = conn.Close()
 			}
 			if err != nil {
-				fmt.Printf("The server %s fail \n", key)
-				delete(registry.AvailableServer, key)
+				fmt.Printf("The server %s fail \n", s)
+				delete(registry.AvailableServer, s)
 			}
 		}
 		//Update the available server list for load balancer
 		if len(registry.LoadBalancerAddress) != 0 {
-			updateLoadBalancer(registry.LoadBalancerAddress, registry.AvailableServer)
+			idleServer = updateLoadBalancer(registry.LoadBalancerAddress, registry.AvailableServer)
 		}
 		time.Sleep(1 * time.Second)
 	}
 }
 
-func updateLoadBalancer(loadBalancer string, updatedList map[string]string) {
-	var flag bool
+func updateLoadBalancer(loadBalancer string, updatedList map[string]string) []string {
+	var idleServer []string
 	lb, err := rpc.Dial("tcp", loadBalancer)
 	if err != nil {
 		fmt.Printf("An error occured %s", err)
 	}
-	err = lb.Call("LoadBalancer.UpdateAvailableServers", updatedList, &flag)
+	err = lb.Call("LoadBalancer.UpdateAvailableServers", updatedList, &idleServer)
 	if err != nil {
 		fmt.Printf("An error occurred %s\n", err)
 	}
+	return idleServer
 }
 
 func playOnRegistry(port string, registry *services.Registry) {
@@ -59,30 +59,6 @@ func playOnRegistry(port string, registry *services.Registry) {
 	go server.Accept(lis)
 }
 
-func getPort() string {
-	var match string
-	file, err := os.Open("Dockerfile")
-	if err != nil {
-		fmt.Printf("Error opening DockerFile %s\n", err)
-		return ""
-	}
-	defer func(file *os.File) {
-		_ = file.Close()
-	}(file)
-	//REX that match the strings in the docker file that correspond to EXPOSE
-	portPattern := regexp.MustCompile(`EXPOSE\s+(\d+)`)
-	//Read file
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		matches := portPattern.FindStringSubmatch(line)
-		if len(matches) > 1 {
-			match = matches[1]
-		}
-	}
-	return match
-}
-
 func GetOutboundIP() net.IP {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
@@ -93,4 +69,12 @@ func GetOutboundIP() net.IP {
 	}(conn)
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
 	return localAddr.IP
+}
+
+func createIdleServerList(availableServer map[string]string) []string {
+	var idle = make([]string, 0)
+	for s := range availableServer {
+		idle = append(idle, s)
+	}
+	return idle
 }
